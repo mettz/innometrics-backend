b337c0a2cfb324148b58d4e8a689e79aedf75edf
Ihar <Igor.shulgan2013@gmail.com>
2019-01-09 12:23:48 +0100

    Fix name

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ from api.constants import INNOMETRICS_PATH, INNOMETRICS_PRODUCTION
 
 config = ConfigParser()
 config_file_name = 'config_production.ini' if INNOMETRICS_PRODUCTION else 'config.ini'
-config.read(os.path.join(INNOMETRICS_PATH, 'config.ini'))
+config.read(os.path.join(INNOMETRICS_PATH, config_file_name))
